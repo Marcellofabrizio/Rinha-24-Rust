3055c379b6acfa23148bb2b60182d6b9408e275c
Marcellofabrizio <marcfabrizio10@gmail.com>
2024-02-23 20:33:10 -0300

    Declares main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-use std::env;
+mod api;
+mod db;
+
+use std::{env, sync::Arc};
+
+use db::repository::PgRepository;
+use api::app_service::app_service;
+
+type AppState = Arc<PgRepository>;
 
 #[tokio::main]
 async fn main() {
@@ -7,7 +15,19 @@ async fn main() {
         .and_then(|port| port.parse::<u16>().ok())
         .unwrap_or(9999);
 
-    let database_url = env::var("DATABASE_URL").unwrap_or(String::from(
-        "postgres://user:pass@localhost:5432/blackhole",
-    ));
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or(String::from("postgres://user:pass@localhost:5432/rinha24"));
+
+    let repo = PgRepository::connect(&database_url)
+        .await
+        .unwrap();
+
+
+    let app_state = Arc::new(repo);
+
+    let app = app_service(app_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+    
 }
